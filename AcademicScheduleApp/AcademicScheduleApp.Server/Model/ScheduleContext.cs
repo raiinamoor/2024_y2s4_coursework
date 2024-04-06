69b37bca2c67fcf18f6b55e3cf3d1052ad60410c
Raii <raiinamoor@gmail.com>
2024-04-06 16:53:17 +0300

    Создать контекст данных для БД расписания

---
+++ AcademicScheduleApp/AcademicScheduleApp.Server/Model/ScheduleContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AcademicScheduleApp.Model.Entities;
+
+namespace AcademicScheduleApp.Model
+{
+    public class ScheduleContext : DbContext
+    {
+        public DbSet<StudentGroup> StudentGroups { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Professor> Professors { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Classroom> Classrooms { get; set; }
+        public DbSet<Class> Classes { get; set; }
+
+        public ScheduleContext(DbContextOptions<ScheduleContext> options) : base(options)
+        {
+            //Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
