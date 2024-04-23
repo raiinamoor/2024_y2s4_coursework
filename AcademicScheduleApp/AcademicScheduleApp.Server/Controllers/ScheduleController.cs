26c7ca2a28e037fcde6de4d81feb57c0b78044a2
Raii <raiinamoor@gmail.com>
2024-04-23 19:45:49 +0300

    Создать ScheduleController с методом получения данных о занятиях

---
+++ AcademicScheduleApp/AcademicScheduleApp.Server/Controllers/ScheduleController.cs
@@ -0,0 +1,32 @@
+﻿using AcademicScheduleApp.Model;
+using AcademicScheduleApp.Model.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+namespace AcademicScheduleApp.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScheduleController : ControllerBase
+    {
+        ScheduleContext _context;
+        public ScheduleController(ScheduleContext context)
+        {
+            _context = context;
+        }
+        [HttpGet(Name="GetClasses")]
+        public IEnumerable<Class> GetClasses(string group = "221-3710")
+        {
+            var classes = _context.Classes
+                .Where(c => c.StudentGroup.Number == group)
+                .Where(c => c.DateBegin <= DateOnly.FromDateTime(DateTime.Now) &&
+                            c.DateEnd >= DateOnly.FromDateTime(DateTime.Now))
+                .Include(c => c.Subject)
+                .Include(c => c.Classroom)
+                .ToArray();
+
+            return classes;
+        }
+    }
+}
