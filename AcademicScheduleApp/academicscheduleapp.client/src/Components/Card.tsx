e0f12906c92897537719094b813660fd4c7898f2
Raii <raiinamoor@gmail.com>
2024-04-23 20:43:57 +0300

    Определять цвет карточки на основе времени

--- AcademicScheduleApp/academicscheduleapp.client/src/Components/Card.tsx
+++ AcademicScheduleApp/academicscheduleapp.client/src/Components/Card.tsx
@@ -4,6 +4,7 @@ export function Card({ thisClass} : {thisClass:Class}) {
     const startValue = timeToNumber(thisClass.timeBegin)-dayStart;
     const endValue = timeToNumber(thisClass.timeEnd)-dayStart;
     const dur = endValue-startValue;
+    const color = colorFromTime(startValue);
     
     return (
         <div className="card bg-red-400" style={{top:startValue, height:dur}}>
@@ -21,3 +22,17 @@ function timeToNumber(time: string) : number {
 }
 
 const dayStart = timeToNumber('9:00:00');
+
+function colorFromTime(time: number) : string {
+    if (time < timeToNumber('10:40:00') - dayStart)
+        return '#fca5a5';
+    else if (time < timeToNumber('12:20:00') - dayStart)
+        return '#fde68a';
+    else if (time < timeToNumber('13:50:00') - dayStart)
+        return '#d9f99d';
+    else if (time < timeToNumber('16:00:00') - dayStart)
+        return '#a5f3fc';
+    else if (time < timeToNumber('19:20:00') - dayStart)
+        return '#818cf8';
+    else return '#c084fc';
+}
