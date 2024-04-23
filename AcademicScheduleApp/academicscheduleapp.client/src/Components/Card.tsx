9694c5cdcbdc510dc6df4b3a8dbae6eec51cd76f
Raii <raiinamoor@gmail.com>
2024-04-23 20:33:39 +0300

    Создать компонент карточки с информацией о занятии

---
+++ AcademicScheduleApp/academicscheduleapp.client/src/Components/Card.tsx
@@ -0,0 +1,10 @@
+export function Card() {
+    
+    return (
+        <div className="card bg-red-400">
+            <b className="p-1 truncate left-1"></b>
+            <p className="p-1 truncate absolute left-1 top-5 text-sm"></p>
+            <p className="p-1 absolute left-1 bottom-0 text-sm"></p>
+        </div>
+    );
+}
