f3e77b69a17a6ac2df0af7a7aae63adc3b4457f4
Raii <raiinamoor@gmail.com>
2024-04-23 20:41:15 +0300

    Добавить prop thisClass для использования данных о занятии в карточке и определения положения в таблице

--- AcademicScheduleApp/academicscheduleapp.client/src/Components/Card.tsx
+++ AcademicScheduleApp/academicscheduleapp.client/src/Components/Card.tsx
@@ -1,10 +1,23 @@
-export function Card() {
+import { Class } from "../Interfaces";
+
+export function Card({ thisClass} : {thisClass:Class}) {
+    const startValue = timeToNumber(thisClass.timeBegin)-dayStart;
+    const endValue = timeToNumber(thisClass.timeEnd)-dayStart;
+    const dur = endValue-startValue;
     
     return (
-        <div className="card bg-red-400">
-            <b className="p-1 truncate left-1"></b>
-            <p className="p-1 truncate absolute left-1 top-5 text-sm"></p>
-            <p className="p-1 absolute left-1 bottom-0 text-sm"></p>
+        <div className="card bg-red-400" style={{top:startValue, height:dur}}>
+            <b className="p-1 truncate left-1">{thisClass.subject.name} ({thisClass.type})</b>
+            <p className="p-1 truncate absolute left-1 top-5 text-sm">{thisClass.classroom.type} {thisClass.classroom.name}</p>
+            <p className="p-1 absolute left-1 bottom-0 text-sm">{thisClass.dateBegin} - {thisClass.dateEnd}</p>
         </div>
     );
 }
+
+
+function timeToNumber(time: string) : number {
+    let [hrs, mins] = time.split(':');
+    return +hrs*60 + +mins;
+}
+
+const dayStart = timeToNumber('9:00:00');
