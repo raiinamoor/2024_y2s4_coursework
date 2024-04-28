755d2d7713a39cac4d350fb4ee7063963ef454b0
Raii <raiinamoor@gmail.com>
2024-04-28 20:56:34 +0300

    Изменить отображение текста в CardDetails

--- AcademicScheduleApp/academicscheduleapp.client/src/Components/CardDetails.tsx
+++ AcademicScheduleApp/academicscheduleapp.client/src/Components/CardDetails.tsx
@@ -11,17 +11,17 @@ export function CardDetails( {thisClass, clickFn} : {thisClass:Class, clickFn:Ca
             <div className="absolute opacity-40 w-full h-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-slate-900" onClick={handleClick}></div>
 
             {/* Card with details */}
-            <div className="card-details w-[400px] h-[300px] transform -translate-x-1/2 -translate-y-1/2 bg-black">
-                <b className="p-5 absolute left-1">
+            <div className="card-details pt-5 text-left w-[400px] h-[300px] transform -translate-x-1/2 -translate-y-1/2 bg-black">
+                <b className="px-6 block left-1">
                     {thisClass.subject.name} ({thisClass.type})
                 </b>
-                <p className="p-6 absolute" style={{top:'10%'}}>
+                <p className="p-6 absolute" style={{top:'15%'}}>
                     {thisClass.dateBegin} - {thisClass.dateEnd}
                 </p>
-                <p className="p-6 absolute" style={{top:'20%'}}>
+                <p className="p-6 absolute" style={{top:'50%'}}>
                     {thisClass.classroom.type} {thisClass.classroom.name}
                 </p>
-                <p className="p-6 absolute top-1/2">
+                <p className="p-6 absolute" style={{top:'65%'}}>
                     {thisClass.professors?.map(p => 
                         `${p.lastName} ${p.firstName[0]}. ${p.secondName[0]}.`
                     ).join(", ")}
