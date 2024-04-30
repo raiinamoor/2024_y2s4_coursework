49f191734d7d42b20e785240b5e0de9e033ace34
Raii <raiinamoor@gmail.com>
2024-04-30 15:59:33 +0300

    Поменять цвет текста в CardDetails на светло-серый

--- AcademicScheduleApp/academicscheduleapp.client/src/Components/CardDetails.tsx
+++ AcademicScheduleApp/academicscheduleapp.client/src/Components/CardDetails.tsx
@@ -11,7 +11,7 @@ export function CardDetails( {thisClass, clickFn} : {thisClass:Class, clickFn:Ca
             <div className="fixed opacity-40 w-full h-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-slate-900" onClick={handleClick}></div>
 
             {/* Card with details */}
-            <div className="card-details pt-5 text-left w-[400px] h-[300px] transform -translate-x-1/2 -translate-y-1/2 bg-black">
+            <div className="card-details text-gray-300 pt-5 text-left w-[400px] h-[300px] transform -translate-x-1/2 -translate-y-1/2 bg-black">
                 <b className="px-6 block left-1">
                     {thisClass.subject.name} ({thisClass.type})
                 </b>
