0a72ff194f4b357df5a89da79c7ee40406bc1f6b
Raii <raiinamoor@gmail.com>
2024-04-30 16:18:48 +0300

    Добавить кнопки для навигации между формами в компонент App

--- AcademicScheduleApp/academicscheduleapp.client/src/App.tsx
+++ AcademicScheduleApp/academicscheduleapp.client/src/App.tsx
@@ -1,12 +1,24 @@
-import './App.css';
+import { useState } from 'react';
 import { Timetable } from './Components/Timetable';
+import { NewClassForm } from './Components/NewClassForm';
+import { CustomButton } from './Components/CustomButton';
+import { RemoveClassForm } from './Components/RemoveClassForm';
+import './App.css';
 
 function App() {
-
+    const [page, setPage] = useState<string>('schedule');
+    
     return ( 
-    <>
-        <Timetable/>
-    </>)
+    <div className='flex flex-col gap-5'>
+        <div className='flex flex-row justify-evenly'>
+            <CustomButton isSelected={page==='schedule'} onClick={()=>setPage('schedule')}>Расписание</CustomButton>
+            <CustomButton isSelected={page==='add'} onClick={()=>setPage('add')}>Добавить занятие</CustomButton>
+            <CustomButton isSelected={page==='remove'} onClick={()=>setPage('remove')}>Удалить занятие</CustomButton>
+        </div>
+        {page === 'schedule' && <Timetable/>}
+        {page === 'add' && <NewClassForm/>}
+        {page === 'remove' && <RemoveClassForm/>}
+    </div>)
     
 }
 
